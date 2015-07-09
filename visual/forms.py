9f5a757138d7e094fb2b149693bb202a9de57fa0
Faiyaaz Khan <faiyaaz.khan13@gmail.com>
2015-07-09 19:32:03 +0200

    Added search functionality

--- visual/forms.py
+++ visual/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 
-class searchForm(forms.Form):
-	star_rating = forms.decimalField(minvalue=0,maxvalue=5)
-	
+class SearchForm(forms.Form):
+	star_rating = forms.IntegerField(min_value=0,max_value=5)
+	review_score = forms.FloatField(min_value=0,max_value=5)
+	no_of_adults = forms.IntegerField(min_value=1,max_value=9)
+	no_of_children = forms.IntegerField(min_value=0,max_value=9)
+	begin_date = forms.SplitDateTimeField()
+	end_date = forms.SplitDateTimeField()
--- visual/views.py
+++ visual/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponseRedirect
+from .forms import SearchForm
 
 class HomeView(generic.TemplateView):
 	template_name = "visual/index.html"
@@ -15,3 +17,22 @@ class FaqView(generic.TemplateView):
 
 def country(request, country): 
 	return render(request, 'visual/index.html')
+
+def search(request):
+	# if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SearchForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # search for data in the database then return the view that will show the list of hotels that match the search
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SearchForm()
+
+    return render(request, 'visual/search.html', {'form': form})
