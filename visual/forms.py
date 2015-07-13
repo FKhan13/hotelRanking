aa28bdbd2d36efa5d2a18927e413b1a55f6ebfb7
TamatiB <pelonomi.moiloa@gmail.com>
2015-07-13 09:48:53 +0200

    Add new forms to forms.py

--- visual/forms.py
+++ visual/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 
 class SearchForm(forms.Form):
-	star_rating = forms.IntegerField(min_value=0,max_value=5)
-	review_score = forms.FloatField(min_value=0,max_value=5)
-	no_of_adults = forms.IntegerField(min_value=1,max_value=9)
-	no_of_children = forms.IntegerField(min_value=0,max_value=9)
-	begin_date = forms.SplitDateTimeField()
-	end_date = forms.SplitDateTimeField()
+    star_rating = forms.IntegerField(min_value=0,max_value=5)
+    no_of_adults = forms.IntegerField(min_value=1,max_value=9)
+    no_of_children = forms.IntegerField(min_value=0,max_value=9)
+    no_of_rooms = forms.IntergerField(min_value = 0, max_value = 10)
+    begin_date = forms.DateField()
+    end_date = forms.DateField()
+      
+      #destination/city id
